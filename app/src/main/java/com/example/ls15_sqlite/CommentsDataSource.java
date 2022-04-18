47aea17f2be12c1242ab2a6b3074802a76ba2101
PlainOldKevin <61211742+PlainOldKevin@users.noreply.github.com>
2022-04-17 22:21:33 -0400

    initial commit

---
+++ app/src/main/java/com/example/ls15_sqlite/CommentsDataSource.java
@@ -0,0 +1,67 @@
+package com.example.ls15_sqlite;
+
+/**
+ * Created by relkharboutly on 3/30/2016.
+ */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommentsDataSource {
+
+    // Database fields
+    private SQLiteDatabase database;
+    private MySQLiteHelper dbHelper;
+    private String[] allColumns = { MySQLiteHelper.COLUMN_ID,
+            MySQLiteHelper.COLUMN_COMMENT };
+
+    public CommentsDataSource(Context context) {
+        dbHelper = new MySQLiteHelper(context);
+    }
+
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+
+    }
+    public  SQLiteDatabase getDatabase() {return database;}
+    public void close(){
+        dbHelper.close();
+    }
+
+    public Comment createComment(String comment) {
+        ContentValues values = new ContentValues();
+        values.put(MySQLiteHelper.COLUMN_COMMENT, comment);
+        long insertID = database.insert(MySQLiteHelper.TABLE_COMMENTS,null,values);
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_COMMENTS,allColumns,MySQLiteHelper.COLUMN_ID + " = " + insertID, null, null, null, null);
+        cursor.moveToFirst();
+        Comment newComment = cursorToComment(cursor);
+        cursor.close();
+
+        return newComment;
+    }
+
+    public void deleteComment(Comment comment) {
+        long id = comment.getId();
+        database.delete(MySQLiteHelper.TABLE_COMMENTS,MySQLiteHelper.COLUMN_ID + " = " + id, null);
+
+    }
+
+    public List<Comment> getAllComments() {
+
+
+        return null;
+    }
+
+    private Comment cursorToComment(Cursor cursor) {
+        Comment comment = new Comment();
+        comment.setId(cursor.getLong(0));
+        comment.setComment(cursor.getString(1));
+
+        return comment;
+    }
+}
---
+++ app/src/main/java/com/example/ls15_sqlite/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ls15_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
